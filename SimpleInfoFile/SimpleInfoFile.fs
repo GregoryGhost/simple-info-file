458ffa2470e22fea391333960af6691d71e48eb0
Gregory <KulakovGV@tomskasu.ru>
2019-03-31 10:08:46 +0700

    Добавил примеры "тестов".

--- SimpleInfoFile/SimpleInfoFile.fs
+++ SimpleInfoFile/SimpleInfoFile.fs
@@ -7,12 +7,23 @@ module SimpleInfoFile =
         {
             CountLines: int
             CountAsciiSymbols: int
-            SizeOfByte: int 
+            SizeInBytes: int 
         }
+
+    type InfoData = string list -> int
+
+    let countLine: InfoData =
+        fun x -> 3
+
+    let calcSizeDataInBytes: InfoData =
+        fun x -> 3
+
+    let countAsciiSymbols: InfoData =
+        fun x -> 3
 
     let getInfoFile (path: string): InfoFile = 
         { 
             CountLines = 0
             CountAsciiSymbols = 0
-            SizeOfByte = 0
+            SizeInBytes = 0
         }
