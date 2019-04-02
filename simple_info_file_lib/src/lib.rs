e56b1539dbe3a9391516f22bc5ae022eec0ad001
Gregory <KulakovGV@tomskasu.ru>
2019-04-02 12:34:13 +0700

    Добавил тест на подсчет количества строк.

--- simple_info_file_lib/src/lib.rs
+++ simple_info_file_lib/src/lib.rs
@@ -1,7 +1,31 @@
+#[path = "lib1.rs"] mod simple_info_file;
+
 #[cfg(test)]
 mod tests {
+    use crate::simple_info_file::simple_info_file::count_line;
+
+    const COMMON_DATA: &'static [&'static str] = &["1235", "123", "test\x7f"];
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn check_calc_count_line() {
+        let actual = count_line(COMMON_DATA);
+        let expected = 3;
+        
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn check_calc_size_data_in_bytes_ascii() {
+        panic!("todo");
+    }
+
+    #[test]
+    fn check_calc_ascii_symbols() {
+        panic!("todo");
+    }
+
+    #[test]
+    fn check_get_info_file() {
+        panic!("todo");
     }
 }
---
+++ simple_info_file_lib/src/simple_info_file.rs
@@ -0,0 +1,17 @@
+pub mod simple_info_file {
+    pub fn count_line(data: &[&str]) -> usize {
+        data.len()
+    }
+
+    fn count_ascii_symbols(data: &str) -> i32 {
+        panic!("todo");
+    }
+
+    fn calc_size_data_in_ascii_bytes(data: &str) -> i32 {
+        panic!("todo");
+    }
+
+    fn get_info_file(data: &str) -> i32 {
+        panic!("todo");
+    }
+}
