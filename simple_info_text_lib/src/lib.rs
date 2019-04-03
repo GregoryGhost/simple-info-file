9230e09b18a23aedd8100a2758144e7dd6440d29
Gregory <KulakovGV@tomskasu.ru>
2019-04-03 13:09:35 +0700

    Переименовал библиотеку по получению инфы о тексте.

--- simple_info_text_lib/src/lib.rs
+++ simple_info_text_lib/src/lib.rs
@@ -1,12 +1,13 @@
-#[path = "simple_info_text.rs"] mod simple_info_text;
+#[path = "simple_info_text.rs"] pub mod simple_info_text;
+
 
 #[cfg(test)]
 mod tests {
-    use crate::simple_info_text::simple_info_text::count_line;
-    use crate::simple_info_text::simple_info_text::count_printable_ascii_symbols;
-    use crate::simple_info_text::simple_info_text::calc_size_data_in_ascii_bytes;
-    use crate::simple_info_text::simple_info_text::get_info_text;
-    use crate::simple_info_text::simple_info_text::InfoText;
+    use crate::simple_info_text::count_line;
+    use crate::simple_info_text::count_printable_ascii_symbols;
+    use crate::simple_info_text::calc_size_data_in_ascii_bytes;
+    use crate::simple_info_text::get_info_text;
+    use crate::simple_info_text::InfoText;
 
     const COMMON_DATA: &'static [&'static str] = &["1235", "123", "test\x7f"];
 
@@ -36,12 +37,6 @@ mod tests {
 
     #[test]
     fn check_get_info_text() {
-        // let mut file = File::open(path).expect("Файл не найден");
-        // let data: Vec<String> = io::BufReader::new(file).lines().map(|line| line.unwrap()).collect();
-        // let data: Vec<&str> = data.iter().map(|line| line as &str).collect();
-        // let data: &[&str] = &data;
-
-        // let path = env::current_dir()?.display().join("test.txt")?.to_str();
         let actual = get_info_text(COMMON_DATA);
         let expected: std::io::Result<InfoText> = Ok(InfoText {
             lines: 3,
