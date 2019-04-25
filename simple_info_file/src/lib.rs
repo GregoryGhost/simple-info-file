e9a32729ec2b9e3c44c3e0924fc7be6cad0a0f0d
Gregory <KulakovGV@tomskasu.ru>
2019-04-25 20:19:52 +0700

    #4 Починил тесты.

--- simple_info_file/src/lib.rs
+++ simple_info_file/src/lib.rs
@@ -7,14 +7,19 @@ mod tests {
     use crate::simple_info_text::get_info_text;
     use crate::simple_info_text::InfoText;
     use crate::file_helper::read_file;
+    
+    use std::path::Path;
+    use std::io::Result;
 
     const PATH_TO_TEST_FILE: &'static str = "test.txt";
 
     #[test]
     fn check_get_info_text() {
-        let data = read_file(PATH_TO_TEST_FILE).unwrap();
+        let path = Path::new(PATH_TO_TEST_FILE);
+        let data = read_file(path).unwrap();
         let actual = get_info_text(&data);
-        let expected: std::io::Result<InfoText> = Ok(InfoText {
+
+        let expected: Result<InfoText> = Ok(InfoText {
             lines: 3,
             printable_ascii_symbols: 14,
             size_in_bytes_ascii: 21
